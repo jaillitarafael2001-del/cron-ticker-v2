bf7ee86dd8762aeb7d4da3b87025c96b3a80bd99
jaillitarafael2001 <jaillitarafael2001@gmail.com>
2026-03-08 23:22:50 -0400

    pruebas test

--- tests/sync.test.js
+++ tests/sync.test.js
@@ -3,7 +3,6 @@ const { syncTest } = require('../tasks/sync');
 describe('Pruebas de Sync Test', () => {
 
     test('debe de ejecutar el proceso 2 veces', () => {
-        syncTest();
         const times = syncTest();
         console.log('Se llamo ', times);
 
